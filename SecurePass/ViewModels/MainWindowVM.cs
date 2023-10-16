03631085a7e5a1555749025036c8a91bc30f9492
Sashok9203 <Kovalchuk.olex@gmail.com>
2023-10-16 20:52:30 +0300

    Добавьте файлы проекта.

---
+++ SecurePass/ViewModels/MainWindowVM.cs
@@ -0,0 +1,14 @@
+﻿using data_access.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecurePass.ViewModels
+{
+    internal class MainWindowVM
+    {
+        private SecurePassDBContext dbContex = new();
+    }
+}
---
+++ data_access/Data/SecurePassDBContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace data_access.Data
+{
+    public class SecurePassDBContext : DbContext
+    {
+        public SecurePassDBContext()
+        {
+            //Database.EnsureDeleted();
+             Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string connect = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=SecurePassDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;Pooling = true";
+            optionsBuilder.UseSqlServer(connect);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //modelBuilder.ApplyConfiguration<MyEntity>(new MyEntityConfig());
+
+
+        }
+
+        //public DbSet<MyEntity> MyEntity { get; set; }
+       
+
+    }
+}
