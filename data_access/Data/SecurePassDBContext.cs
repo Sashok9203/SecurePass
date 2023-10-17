041f95a5634634646f832d5e519faf31f241643f
Sashok9203 <Kovalchuk.olex@gmail.com>
2023-10-17 23:26:32 +0300

    Create entity Category for database

--- data_access/Data/SecurePassDBContext.cs
+++ data_access/Data/SecurePassDBContext.cs
@@ -23,6 +23,7 @@ namespace data_access.Data
         {
             //modelBuilder.ApplyConfiguration<MyEntity>(new MyEntityConfig());
             modelBuilder.ApplyConfiguration<User>(new UserConfig());
+            modelBuilder.ApplyConfiguration<Category>(new CategoryConfig());
 
             ///...
 
@@ -32,6 +33,7 @@ namespace data_access.Data
 
         //public DbSet<MyEntity> MyEntity { get; set; }
           public DbSet<User> Users { get; set; }
+          public DbSet<Category> Categories { get; set; }
 
     }
 }
---
+++ data_access/Entities/Configs/CategoryConfig.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace data_access.Entities.Configs
+{
+    internal class CategoryConfig : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Name).HasMaxLength(56);
+            builder.HasIndex(x => x.Name).IsUnique();
+            builder.ToTable(t => t.HasCheckConstraint("Name_check", "[Name] <> ''"));
+        }
+    }
+}
