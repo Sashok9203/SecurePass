43adf9fbbae7ef1c51f11af7efe50930268d439b
Sashok9203 <Kovalchuk.olex@gmail.com>
2023-10-19 00:03:04 +0300

    Unnecessary usings removed

--- data_access/Entities/CreditCard.cs
+++ data_access/Entities/CreditCard.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-
-namespace data_access.Entities
+﻿namespace data_access.Entities
 {
     public class CreditCard:SecureObject
     {
