8dd73881b843265ecc03b17a5d985e7784b0f9ee
Sashok9203 <Kovalchuk.olex@gmail.com>
2023-10-18 17:27:08 +0300

    Update

--- data_access/Entities/Universal.cs
+++ data_access/Entities/Universal.cs
@@ -15,6 +15,6 @@ namespace data_access.Entities
         public int UserId { get; set; }
         public User User { get; set; }
         public int CategoryId { get; set; }
-        public  Category Category { get; set; }
+        public Category Category { get; set; }
     }
 }
