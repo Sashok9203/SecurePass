245c7ef57c121dd3987674317654156e94390fa6
Kllsel <cdarina957@gmail.com>
2023-10-19 21:59:20 +0300

    empty space deleted, Title in serverconfig added

--- data_access/Entities/Category.cs
+++ data_access/Entities/Category.cs
@@ -6,6 +6,5 @@
         public ICollection<Universal> Universals { get; set; } = new HashSet<Universal>();
         public ICollection<CreditCard> CreditCards { get; set;} = new HashSet<CreditCard> ();
         public ICollection<Server> Servers { get; set;} = new HashSet<Server> ();
-
     }
 }
--- data_access/Entities/Configs/ServerConfig.cs
+++ data_access/Entities/Configs/ServerConfig.cs
@@ -10,6 +10,7 @@ namespace data_access.Entities.Configs
             builder.HasKey(x => x.Id);
             builder.Property(x => x.ImageId);
             builder.HasOne(x => x.User).WithMany(x => x.Servers);
+            builder.Property(x => x.Title).HasMaxLength(128);
             builder.HasOne(x => x.Category).WithMany(x => x.Servers);
             builder.Property(x => x.URL).HasMaxLength(128);
             builder.Property(x => x.Name).HasMaxLength(128);
