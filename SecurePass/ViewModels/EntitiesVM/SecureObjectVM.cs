e6546cfcd32292eb71e2d275b2affb0239808501
Sashok9203 <Kovalchuk.olex@gmail.com>
2023-10-24 12:06:39 +0300

    Entities base classes added

---
+++ SecurePass/ViewModels/EntitiesVM/SecureObjectVM.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.RightsManagement;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecurePass.ViewModels.EntitiesVM
+{
+    internal class SecureObjectVM : BaseEntityVM
+    {
+        private int categoryId;
+        private string title;
+
+        public SecureObjectVM(int id,int imageId,int categoryId,int userId,string title) : base(id ,imageId)
+        {
+            this.title = title;
+            this.UserId = userId;
+            this.categoryId = categoryId;
+        }
+
+        public string Title
+        {
+            get => title;
+            set 
+            { 
+                title = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public int CategoryId
+        {
+            get => categoryId;
+            set
+            {
+                categoryId = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public int UserId { get; set; }
+    }
+}
--- data_access/Entities/Category.cs
+++ data_access/Entities/Category.cs
@@ -2,11 +2,11 @@
 {
     public class Category : BaseEntity
     {
-        public string? Name { get; set; }
+        public string Name { get; set; }
         public ICollection<Universal> Universals { get; set; } = new HashSet<Universal>();
         public ICollection<CreditCard> CreditCards { get; set;} = new HashSet<CreditCard> ();
         public ICollection<Email> Emails { get; set; } = new HashSet<Email> ();
         public ICollection<Server> Servers { get; set;} = new HashSet<Server> ();
-        public ICollection<BankAccount> BankAccounts { get; set; } = new HashSet<BankAccount>;
+        public ICollection<BankAccount> BankAccounts { get; set; } = new HashSet<BankAccount>();
     }
 }
