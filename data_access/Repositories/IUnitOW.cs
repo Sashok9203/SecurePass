fea514855c07f0aa4c02ca5a63b7fbf3cb9ca29a
Kholod13 <111706893+Kholod13@users.noreply.github.com>
2023-10-27 00:25:10 +0300

    finalized UoW

---
+++ data_access/Repositories/IUnitOW.cs
@@ -0,0 +1,24 @@
+﻿using data_access.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace data_access.Repositories
+{
+    public interface IUnitOW : IDisposable
+    {
+        IRepository<BankAccount> BankAccounts { get; }
+        IRepository<BaseEntity> BaseEntities { get; }
+        IRepository<Category> Categories { get; }
+        IRepository<CreditCard> CreditCards { get; }
+        IRepository<DataBase> DataBases { get; }
+        IRepository<Email> Emails { get; }
+        IRepository<SecureObject> SecureObjects { get; }
+        IRepository<Server> Servers { get; }
+        IRepository<Universal> Universals { get; }
+        IRepository<User> Users { get; }        
+        void Save();
+    }
+}
