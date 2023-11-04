65fb7d3143ba0e2900eed9384975fe33fc3161e2
ArtemEliseev1 <124588610+ArtemEliseev1@users.noreply.github.com>
2023-11-04 23:09:48 +0200

    change icon size and massage launguage

--- SecurePass/ViewModels/MainWindowVM.cs
+++ SecurePass/ViewModels/MainWindowVM.cs
@@ -98,7 +98,7 @@ namespace SecurePass.ViewModels
 
         private void clearData()
         {
-            MessageBoxResult result = MessageBox.Show("Чи ви впевнені, що бажаєте вийти з аккаунту?", "Підтвердження виходу", MessageBoxButton.YesNo, MessageBoxImage.Question);
+            MessageBoxResult result = MessageBox.Show("Are you sure you want to log out?", "Logout confirmation", MessageBoxButton.YesNo, MessageBoxImage.Question);
 
             if (result == MessageBoxResult.Yes)
             {
