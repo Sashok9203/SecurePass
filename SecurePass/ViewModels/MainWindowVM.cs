ae1b5f92e984c560959cf77a2a88fc25f7a662ac
Sashok9203 <Kovalchuk.olex@gmail.com>
2023-11-05 15:35:06 +0200

    Fix

--- SecurePass/ViewModels/MainWindowVM.cs
+++ SecurePass/ViewModels/MainWindowVM.cs
@@ -103,7 +103,6 @@ namespace SecurePass.ViewModels
         private void clearData()
         {
             MessageBoxResult result = MessageBox.Show("Are you sure you want to log out?", "Logout confirmation", MessageBoxButton.YesNo, MessageBoxImage.Question);
-
             if (result == MessageBoxResult.Yes)
             {
                 createdObject = null;
